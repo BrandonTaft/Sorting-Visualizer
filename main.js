eaf00ac6a82a56cf5aefd7d95508ec9594bfa509
Btaft6278 <btaft6278@gmail.com>
2022-04-03 20:10:36 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function createArray(){
+const max = 100;
+const x = [];
+
+for (let i = 0; i < max;) {
+    let num = Math.floor(Math.random() * max + 1)
+    while (x.includes(num) === false) {
+        const container = document.getElementById('container');
+        const bar = document.createElement('div');
+        bar.innerText = num;
+        bar.classList.add('bar');
+        bar.style.width = num + "%"
+        container.appendChild(bar)
+        x.push(num)
+        i++
+    }
+}
+}
+//console.log(x)
+    //return (x)
+
+
+//console.log(makeArr(max))
