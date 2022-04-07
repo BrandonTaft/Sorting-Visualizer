dcf143d49406b9ecc4ebd808d00a08cbd7efdafc
Btaft6278 <btaft6278@gmail.com>
2022-04-07 10:49:57 -0400

    finished bubble sort

--- main.js
+++ main.js
@@ -20,12 +20,13 @@ for (let i = 0; i < max;) {
 
 function swap(el1,el2)
 {
-const style1 = window.getComputedStyle(el1);
-const style2 = window.getComputedStyle(el2);
-const transform1 = style1.getPropertyValue("height");
-const transform2 = style2.getPropertyValue("height");
-el1.style.height = transform2;
-el2.style.height = transform1;
+    const newBar = document.getElementsByClassName('bar')
+const style1 = window.getComputedStyle(newBar[j]);
+const style2 = window.getComputedStyle(newBar[j+1]);
+const transform1 = style1.getPropertyValue("width");
+const transform2 = style2.getPropertyValue("width");
+el1.style.width = transform2;
+el2.style.width = transform1;
 }
 
 function test(){
@@ -39,30 +40,75 @@ function test(){
     }
     
 
+// async function bubbleSort() {
+//     const newBar = document.getElementsByClassName('bar')
+//     console.log(arr)
+//     let len = arr.length;
+//     for (let i = 0; i < len; i++) {
+//         await new Promise(resolve => setTimeout(resolve, 100));
+//         for (let j = 0; j < len; j++) {
+            
+//             newBar[j].style.backgroundColor="red" //The element we are testing turns red
+           
+//             await new Promise(resolve => setTimeout(resolve, 100));
+
+//             if (arr[j] < arr[j + 1]) {
+//                 newBar[j+1].style.backgroundColor="blue" //The element we are comparing to turns blue
+//                 //await new Promise(resolve => setTimeout(() => {resolve(), delay(2)}));
+//                 await new Promise(resolve => setTimeout(resolve,100));
+//                 let tmp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = tmp;
+                    
+//                     newBar[j].innerText=arr[j]
+//                     newBar[j].style.width=arr[j]+"%"
+//                     newBar[j+1].innerText=arr[j+1]
+//                     newBar[j+1].style.width=arr[j+1]+"%"
+                   
+//                     //newBar[i].style.backgroundColor="yellowgreen"
+//             } newBar[j].style.backgroundColor="greenyellow"
+        
+//         }
+//     }
+//     return  arr
+// };
+
 async function bubbleSort() {
-    const newBar = document.getElementsByClassName('bar')
-    let len = arr.length;
-    for (let i = 0; i < len; i++) {
-        await new Promise(resolve => setTimeout(resolve, 500));
+    const newBar = document.getElementsByClassName('bar');
+    const len = arr.length;
+    let checked;
+
+    do{
+        checked = false
+    // for (let i = 0; i < len; i++) {
+    //     await new Promise(resolve => setTimeout(resolve, 100));
         for (let j = 0; j < len; j++) {
-            await new Promise(resolve => setTimeout(resolve, 500));
-            if (arr[j] > arr[j + 1]) {
+            
+            newBar[j].style.backgroundColor="red" //The element we are testing turns red
+           
+            await new Promise(resolve => setTimeout(resolve, 100));
+
+            if (arr[j] < arr[j + 1]) {
+                newBar[j+1].style.backgroundColor="blue" //The element we are comparing to turns blue
                 //await new Promise(resolve => setTimeout(() => {resolve(), delay(2)}));
-                await new Promise(resolve => setTimeout(resolve,500));
+                await new Promise(resolve => setTimeout(resolve,100));
                 let tmp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = tmp;
-                    console.log(arr[j], tmp)
-                    newBar[i].innerText=arr[j+1]
-                    newBar[i+1].innerText=arr[j]
-                    newBar[j].style.width=tmp+"%"
-            }
+                    
+                    newBar[j].innerText=arr[j]
+                    newBar[j].style.width=arr[j]+"%"
+                    newBar[j+1].innerText=arr[j+1]
+                    newBar[j+1].style.width=arr[j+1]+"%"
+                   checked = true
+                    //newBar[i].style.backgroundColor="yellowgreen"
+            } newBar[j].style.backgroundColor="greenyellow"
         
         }
-    }
-    console.log(arr)
+   // }
+}while (checked)
+    return  arr
 };
-
 
 
 
