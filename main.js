017540443421c9c0eeb5e9b6ad40522ea439c947
Btaft6278 <btaft6278@gmail.com>
2022-04-11 18:47:02 -0400

    insertion sort

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //************* CREATE NEW RANDOM ARRAY **************** */
 
-const max = 5;
+const max = 10;
 const arr = [];
 
 for (let i = 0; i < max;) {
@@ -172,8 +172,37 @@ function partition(arr, start, end){
 
 //************* PERFORM MERGE SORT **************** */
 
+function merge(left, right) {
+    const newBar = document.getElementsByClassName('bar');
+    let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    
+    // Concatenating the leftover elements
+    // (in case we didn't go through the entire left or right array)
+    return [ ...arr, ...left, ...right ]
+}
 
-
+function mergeSort() {
+  const newBar = document.getElementsByClassName('bar');
+  const half = arr.length / 2
+  // Base case or terminating case
+  if(arr.length < 2){
+    return arr 
+  }
+  
+  const left = arr.splice(0, half)
+  newBar[left.length-1].classList.add('left');
+  return merge(mergeSort(left),mergeSort(arr))
+}
+  
 //************* REFRESH THE ARRAY **************** */
 
 function newArray() {
